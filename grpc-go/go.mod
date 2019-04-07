f8c741efed8e22c5c4c92546479160f5a1054278
tsingsonqin <tsingson@gmail.com>
2019-04-08 07:31:29 +0800

    Initial commit

---
+++ grpc-go/go.mod
@@ -0,0 +1,10 @@
+module github.com/tsingson/android/grpc-go
+
+go 1.12
+
+require (
+	github.com/TakuSemba/grpc-go v0.0.0-20180203085820-32c5e32fb535
+	github.com/golang/protobuf v1.3.1
+	golang.org/x/net v0.0.0-20190328230028-74de082e2cca
+	google.golang.org/grpc v1.19.1
+)
---
+++ grpc-go/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	pb "github.com/TakuSemba/grpc-go/proto"
+)
+
+const (
+	port = ":8080"
+)
+
+type server struct{}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+func (s *server) Order(ctx context.Context, in *pb.CoffeeRequest) (*pb.CoffeeResponse, error) {
+	return &pb.CoffeeResponse{
+		Price: 560,
+		Name: in.Name,
+		Message: "Thank you for ordering " + in.Name,
+	}, nil
+}
+
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterHelloServer(s, &server{})
+	pb.RegisterCoffeeServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
